7b902c35651517c6ea26705d0c9459abc996f156
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-07-26 14:15:06 +0900

    Feat

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from service.config.qdrant_config import init_qdrant_collection
+from service.config.qdrant_singleton import client
 
-app = Flask(__name__)
 
-@app.route('/ai', methods=['POST'])
-def callAi():
-    # DB primary key
-    user_id = request.json["user_id"]
-    return 0
+def create_app():
+    app = Flask(__name__)
+
+    # Qdrant ddl-auto = create로 설정
+    init_qdrant_collection(client)
+
+    # 컨트롤러 1
+    @app.route('/ai', methods=['POST'])
+    def callAi():
+        # DB primary key
+        user_id = request.json["user_id"]
+        return 0
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5050)
--- service/rag.py
+++ service/rag.py
@@ -1,8 +1,26 @@
+import os
+from service.config.qdrant_singleton import client
+from qdrant_client.models import SearchRequest, PointStruct, Filter, FieldCondition, MatchValue
+from dotenv import load_dotenv
 from services.tag import get_user_tags
 from services.review import get_user_reviews
 from services.history import get_user_class_titles
+from service.template.sql_template import FilterOpenClass
+from service config.db import db
 from transformers import AutoTokenizer, AutoModel
+from datetime import datetime
+import torch
 
+# Embedding part
+load_dotenv()
+MODEL_NAME = os.environ.get("DEBERTA_MODEL_NAME")
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModel.from_pretrained(MODEL_NAME)
+model.eval()
+
+# Vector Search part
+COLLECTION_NAME = os.environ.get("QDRANT_COLLECTION_NAME")
 
 def vector_embed(user_id):
     # 1. 사용자 정보 수집
@@ -22,4 +40,31 @@ def vector_embed(user_id):
     # 프롬프트 반환, call_ai 위임
     return answer
 
+
+# 문장 기반 Embed 된 데이터 쿼리. 유사도 추출
 def embed_text(text: str) -> list[float]:
+    inputs = tokenizer(text, return_tensors="pt",
+                       truncation=True, padding=True, max_length=512)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    return outputs.last_hidden_state[:, 0, :].squeeze().tolist()
+
+# 비슷한 클래스 서치
+def search_similar_class(query_vector:
+                         list[float], top_k: int = 5) -> list[dict]:
+    search_result = client.search(
+        collection_name = COLLECTION_NAME,
+        query_vector=query_vector,
+        limit=top_k,
+        with_payload=True
+    )
+
+    return [
+        {
+            "class_id": point.payload.get("class_id"),
+            "title": point.payload.get("title"),
+            "description": point.payload.get("description"),
+            "score": point.score
+        }
+        for point in search_result
+    ]
