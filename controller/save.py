c9a0685e0df3d6e10d4e2ebb70466410ea4a0fe0
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-08-05 17:12:24 +0900

    Feat: 유효하지 않은 필드명과 NotNull Validation 로직 구현

--- controller/save.py
+++ controller/save.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, request, jsonify, Response
 import json
 from collections import OrderedDict
-
-from pydantic import ValidationError
+from typing import Optional
+from pydantic import BaseModel, ValidationError
 from service.data_embed import TagAndClassDataEmbedding, ReviewDataEmbedding
 import datetime
 from controller.response_builder import ResponseBuilder
@@ -10,9 +10,9 @@ from controller.response_builder import ResponseBuilder
 save_bp = Blueprint("rcmd", __name__, url_prefix="/ai/save")
 
 class SaveWithoutReviewReq(BaseModel):
-      class_id: int
-      tag: list[str]
-      class_name: str
+      class_id: Optional[int]
+      class_name: Optional[str]
+      tag: Optional[list[str]]
 
       def getClassId(self):
             return self.class_id
@@ -20,8 +20,8 @@ class SaveWithoutReviewReq(BaseModel):
             return self.tag
       def getClassName(self):
             return self.class_name
-
-class SaveReview:
+      
+class SaveReview(BaseModel):
       class_id: int
       user_id: int
       review: str
@@ -39,47 +39,64 @@ def save_without_review():
       try :
             data = SaveWithoutReviewReq(**request.json)
       except ValidationError as e:
-            return Response(
-                  json.dumps(jsonify(
+            body = OrderedDict([
+            ("class_id", request.json.get("class_id")),
+            ("class_name", request.json.get("class_name")),
+            ("tag", request.json.get("tag"))
+            ])
+            return Response(json.dumps(
                         ResponseBuilder()
                         .is_success(False)
                         .code("Flask_Invalid_Request_400")
                         .http_status(400)
                         .message("class_id, tag, class_name은 모두 필수값입니다.")
-                        .data(data)
-                        .time_stamp(datetime.datetime.now().isoformat)
-                  ), ensure_ascii=False),
-                  status=400,
-                  mimetype="application/json"
-            )
-      
+                        .data(body)
+                        .time_stamp()
+                        .build(), ensure_ascii=False),
+                        status=400,
+                        mimetype="application/json")
+      except UnboundLocalError as e:
+            body = OrderedDict([
+            ("class_id", request.json.get("class_id")),
+            ("class_name", request.json.get("class_name")),
+            ("tag", request.json.get("tag"))
+            ])
+            return Response(json.dumps(
+                        ResponseBuilder()
+                        .is_success(False)
+                        .code("Flask_Invalid_Request_400")
+                        .http_status(400)
+                        .message("유효하지 않은 필드명입니다.")
+                        .data(None)
+                        .time_stamp()
+                        .build(), ensure_ascii=False),
+                        status=400,
+                        mimetype="application/json")
 
 
       body = OrderedDict([
             ("class_id", data.getClassId()),
-            ("tag", data.getTag()),
             ("class_name", data.getClassName()),
+            ("tag", data.getTag())
       ])
     
       embed = TagAndClassDataEmbedding()
 
-      embed.save(data.getClassId(), data.getTag(), data.getClassName())
+      embed.save(data.getClassId(), data.getClassName(), data.getTag())
       
       
       
-      return Response(
-            json.dumps(jsonify(
-                        ResponseBuilder()
-                        .is_success(True)
-                        .code("Flask_201")
-                        .http_status(201)
-                        .message("성공적으로 Qdrant Database에 저장되었습니다.")
-                        .data(body)
-                        .time_stamp(datetime.datetime.now().isoformat)
-                  ), ensure_ascii=False),
-            status=201,
-            mimetype="application/json"
-      )
+      return Response(json.dumps(
+                  ResponseBuilder()
+                  .is_success(True)
+                  .code("Flask_201")
+                  .http_status(201)
+                  .message("성공적으로 Qdrant Database에 저장되었습니다.")
+                  .data(body)
+                  .time_stamp()
+                  .build(), ensure_ascii=False),
+                  status=201,
+                  mimetype="application/json")
 
 @save_bp.route("/review", methods=["POST"])
 def save_review():
@@ -87,17 +104,42 @@ def save_review():
       try:
             data = SaveReview(**request.json)
       except ValidationError as e:
-            return Response(jsonify(
+            body = OrderedDict([
+            ("class_id", request.json.get("class_id")),
+            ("user_id", request.json.get("user_id")),
+            ("review", request.json.get("review"))
+            ])
+            return Response(json.dumps(
                   ResponseBuilder()
                   .is_success(False)
                   .code("Flask_Invalid_Request_400")
                   .http_status(400)
                   .message("class_id, user_id, review는 필수값입니다.")
-                  .data(data)
-                  .time_stamp(datetime.datetime.now().isoformat)
+                  .data(body)
+                  .time_stamp()
+                  .build()
             ), ensure_ascii=False,
             status=400,
             mimetype="application/json")
+      except UnboundLocalError as e:
+            body = OrderedDict([
+            ("class_id", request.json.get("class_id")),
+            ("user_id", request.json.get("user_id")),
+            ("review", request.json.get("review"))
+            ])
+            return Response(json.dumps(
+                  ResponseBuilder()
+                  .is_success(False)
+                  .code("Flask_Invalid_Request_400")
+                  .http_status(400)
+                  .message("유효하지 않은 필드명입니다.")
+                  .data(body)
+                  .time_stamp()
+                  .build()
+            ), ensure_ascii=False,
+            status=400,
+            mimetype="application/json")
+
             
       body = OrderedDict([
             ("class_id", data.getClassId()),
@@ -108,19 +150,16 @@ def save_review():
       embed = ReviewDataEmbedding()
 
       embed.save(data.getClassId(), data.getUserId(), data.getReview())
-
-      response_data = {
-            "isSuccess": True,
-            "code": "201",
-            "httpStatus": 201,
-            "message": "성공적으로 Qdrant Database에 저장되었습니다.",
-            "data": body,
-            "timeStamp": datetime.datetime.now().isoformat()
-      }
-
       
       return Response(
-            json.dumps(response_data, ensure_ascii=False),
+            json.dumps(ResponseBuilder()
+                       .is_success(True)
+                       .code("FLASK_201")
+                       .http_status(201)
+                       .message("성공적으로 Qdrant Database에 저장되었습니다.")
+                       .data(body)
+                       .time_stamp()
+                       .build(), ensure_ascii=False),
             status=201,
             mimetype="application/json"
       )
