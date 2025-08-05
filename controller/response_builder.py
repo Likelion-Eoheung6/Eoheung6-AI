ec6fa6e83374bc27bcd0f86338825ae07f99f4c7
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-08-05 16:17:04 +0900

    Feat: Req DTO 파싱 실패 시 예외 명시 및 Response 빌더 패턴 정의

---
+++ controller/response_builder.py
@@ -0,0 +1,32 @@
+import datetime
+
+class ResponseBuilder:
+    def __init__(self):
+        self._data = {}
+    
+    def is_success(self, is_success: bool):
+        self._data["isSuccess"] = is_success
+        return self
+
+    def code(self, code: str):
+        self._data["code"] = code
+        return self
+
+    def http_status(self, http_status: int):
+        self._data["httpStatus"] = http_status
+        return self
+
+    def message(self, message: str):
+        self._data["message"] = message
+        return self
+
+    def data(self, data):
+        self._data["data"] = data
+        return self
+
+    def time_stamp(self):
+        self._data["timeStamp"] = datetime.datetime.now().isoformat()
+        return self
+
+    def build(self):
+        return self._data
--- controller/save.py
+++ controller/save.py
@@ -1,45 +1,82 @@
 from flask import Blueprint, request, jsonify, Response
 import json
 from collections import OrderedDict
+
+from pydantic import ValidationError
 from service.data_embed import TagAndClassDataEmbedding, ReviewDataEmbedding
 import datetime
+from controller.response_builder import ResponseBuilder
 
 save_bp = Blueprint("rcmd", __name__, url_prefix="/ai/save")
 
+class SaveWithoutReviewReq(BaseModel):
+      class_id: int
+      tag: list[str]
+      class_name: str
+
+      def getClassId(self):
+            return self.class_id
+      def getTag(self):
+            return self.tag
+      def getClassName(self):
+            return self.class_name
+
+class SaveReview:
+      class_id: int
+      user_id: int
+      review: str
+
+      def getClassId(self):
+            return self.class_id
+      def getUserId(self):
+            return self.user_id
+      def getReview(self):
+            return self.review
 
 @save_bp.route("/withoutReview", methods=["POST"])
 def save_without_review():
       print("withoutReview 컨트롤러 진입")
-      class_id = request.json.get("class_id")
-      # user_id = request.json.get("user_id")
-      tag = request.json.get("tag")
-      class_name = request.json.get("class")
-      # 리뷰는 다른 컨트롤러에서 컨트롤할 예정
-      # review = request.json.get("review")
+      try :
+            data = SaveWithoutReviewReq(**request.json)
+      except ValidationError as e:
+            return Response(
+                  json.dumps(jsonify(
+                        ResponseBuilder()
+                        .is_success(False)
+                        .code("Flask_Invalid_Request_400")
+                        .http_status(400)
+                        .message("class_id, tag, class_name은 모두 필수값입니다.")
+                        .data(data)
+                        .time_stamp(datetime.datetime.now().isoformat)
+                  ), ensure_ascii=False),
+                  status=400,
+                  mimetype="application/json"
+            )
+      
 
 
       body = OrderedDict([
-            ("class_id", class_id),
-            ("tag", tag),
-            ("class_name", class_name),
+            ("class_id", data.getClassId()),
+            ("tag", data.getTag()),
+            ("class_name", data.getClassName()),
       ])
     
       embed = TagAndClassDataEmbedding()
 
-      embed.save(class_id, tag, class_name)
+      embed.save(data.getClassId(), data.getTag(), data.getClassName())
       
-      response_data = {
-            "isSuccess": True,
-            "code": "201",
-            "httpStatus": 201,
-            "message": "성공적으로 Qdrant Database에 저장되었습니다.",
-            "data": body,
-            "timeStamp": datetime.datetime.now().isoformat()
-      }
-
+      
       
       return Response(
-            json.dumps(response_data, ensure_ascii=False),
+            json.dumps(jsonify(
+                        ResponseBuilder()
+                        .is_success(True)
+                        .code("Flask_201")
+                        .http_status(201)
+                        .message("성공적으로 Qdrant Database에 저장되었습니다.")
+                        .data(body)
+                        .time_stamp(datetime.datetime.now().isoformat)
+                  ), ensure_ascii=False),
             status=201,
             mimetype="application/json"
       )
@@ -47,20 +84,30 @@ def save_without_review():
 @save_bp.route("/review", methods=["POST"])
 def save_review():
       print("review 컨트롤러 진입")
-
-      class_id = request.json.get("class_id")
-      user_id = request.json.get("user_id")
-      review = request.json.get("review")
-
+      try:
+            data = SaveReview(**request.json)
+      except ValidationError as e:
+            return Response(jsonify(
+                  ResponseBuilder()
+                  .is_success(False)
+                  .code("Flask_Invalid_Request_400")
+                  .http_status(400)
+                  .message("class_id, user_id, review는 필수값입니다.")
+                  .data(data)
+                  .time_stamp(datetime.datetime.now().isoformat)
+            ), ensure_ascii=False,
+            status=400,
+            mimetype="application/json")
+            
       body = OrderedDict([
-            ("class_id", class_id),
-            ("user_id", user_id),
-            ("review", review)
+            ("class_id", data.getClassId()),
+            ("user_id", data.getUserId()),
+            ("review", data.getReview())
       ])
 
       embed = ReviewDataEmbedding()
 
-      embed.save(class_id, user_id, review)
+      embed.save(data.getClassId(), data.getUserId(), data.getReview())
 
       response_data = {
             "isSuccess": True,
